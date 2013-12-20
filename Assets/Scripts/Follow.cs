3b49c1820b40cfc94911230ee4df2260a3cb7ed7
Valerian Cubero <nekith@gmail.com>
2013-12-20 19:20:45 +0100

    Movement on sphere, hover effect

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour
+{
+	public Transform center;
+	public Transform target;
+	public float distance = 0.5f;
+
+	void FixedUpdate()
+	{
+		Vector3 relative = target.position - center.position;
+		transform.position = target.position + relative * distance;
+		transform.LookAt(target.position);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	public SphereCollider layer;
+	public Transform test;
+	public float translateSpeed = 50.0f;
+	public float rotateSpeed = 7.0f;
+	public float gravity = 5.0f;
+
+	void FixedUpdate()
+	{
+		Rotate();
+		Translate();
+	}
+
+	void Rotate()
+	{
+		transform.Rotate(new Vector3(0f, 0f, -Input.GetAxis("Mouse X")) * rotateSpeed);
+	}
+
+	void Translate()
+	{
+		Vector3 center = transform.position - layer.transform.position;
+		center.Normalize();
+		rigidbody.AddForce(center * -gravity * Time.fixedDeltaTime);
+		transform.RotateAround(layer.transform.position, transform.TransformDirection(Vector3.down), Input.GetAxis("Horizontal") * translateSpeed * Time.fixedDeltaTime);
+		transform.RotateAround(layer.transform.position, transform.TransformDirection(Vector3.right), Input.GetAxis("Vertical") * translateSpeed * Time.fixedDeltaTime);
+		Transform cannon = transform.FindChild("Cannon");
+		if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) {
+			cannon.localEulerAngles = new Vector3(Input.GetAxis("Vertical") * 9.0f, -Input.GetAxis("Horizontal") * 9.0f, 0f);
+		} else {
+			cannon.localEulerAngles = new Vector3();
+		}
+	}
+}
