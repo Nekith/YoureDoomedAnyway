71a3db55e6c168b8558323607aa2193d163fcf29
Valerian Cubero <nekith@gmail.com>
2013-12-22 18:29:54 +0100

    HUD

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -20,13 +20,14 @@ public class HUD : MonoBehaviour
 
 	void OnGUI()
 	{
-		GUI.Label(new Rect(Screen.width - 140, 00, 130, 30), timer.ToString("F0"));
+		GUI.skin.label.alignment = TextAnchor.MiddleRight;
+		GUI.Label(new Rect(10, 00, 130, 30), timer.ToString("F0"));
         if (player.energy > 0) {
 			GUI.skin.label.font = font;
-			GUI.Label(new Rect(Screen.width - 140, Screen.height - 60, 130, 30), "Energy");
-			GUI.Label(new Rect(Screen.width - 140, Screen.height - 30, 130, 30), "Exposure");
-			GUI.Label(new Rect(Screen.width - 210, Screen.height - 60, 70, 30), string.Format("{0}", player.energy));
-			GUI.Label(new Rect(Screen.width - 210, Screen.height - 30, 70, 30), string.Format("{0}", player.exposure));
+			GUI.Label(new Rect(80, Screen.height - 60, 130, 30), "Energy");
+			GUI.Label(new Rect(80, Screen.height - 30, 130, 30), "Exposure");
+			GUI.Label(new Rect(10, Screen.height - 60, 70, 30), string.Format("{0}", player.energy));
+			GUI.Label(new Rect(10, Screen.height - 30, 70, 30), string.Format("{0}", player.exposure));
 		}
 	}
 }
